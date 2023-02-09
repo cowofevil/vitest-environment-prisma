a46b05a6c5b708e82bd3c1a0ad84c92009c75245
Ryan Gard <ryan.a.gard@msn.com>
2023-02-09 11:56:39 -0800

    feat(sqlite): add environment option
    Added the 'prismaEnvVarName' environment option to override the environment
    variable name used for Prisma connections which is necessary for situations
    where the variable is not the standard "DATABASE_URL" default. As an example
    when using PlanteScale as a database provider the connection variable needs
    to be "PLANETSCALE_PRISMA_DATABASE_URL".
    
    Updated the README to document the new environment option as well as make
    some minor edits related to SQLite support.

--- src/adapters/sqliteAdapter.ts
+++ src/adapters/sqliteAdapter.ts
@@ -1,7 +1,7 @@
-import { readFileSync, rm } from 'fs'
 import { resolve } from 'path'
 import { promisify } from 'util'
 import { exec } from 'child_process'
+import { readFileSync, rm } from 'fs'
 
 import { EnvironmentAdapterOptions, EnvironmentDatabaseCredentials, SqliteEnvironmentAdapterOptions } from '../@types'
 
